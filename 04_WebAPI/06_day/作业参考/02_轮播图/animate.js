49539b2076128fa8f6f566456c8403e45752afd9
ifer-itcast <yangkangkang@itcast.cn>
2019-05-14 23:20:13 +0800

    web API 第六天

--- 04_WebAPI/06_day/作业参考/02_轮播图/animate.js
+++ 04_WebAPI/06_day/作业参考/02_轮播图/animate.js
@@ -2,7 +2,7 @@ function animate(obj, target, fn) {
     clearInterval(obj.timer);
     var speed = 0;
     obj.timer = setInterval(function() {
-        speed = (target - obj.offsetLeft) / 7;
+        speed = (target - obj.offsetLeft) / 30;
         speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
 
         obj.style.left = obj.offsetLeft + speed + 'px';
